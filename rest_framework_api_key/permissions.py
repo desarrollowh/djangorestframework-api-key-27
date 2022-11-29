34d0727af6bab869c631ec105cc495b12218b0ef
roger-arjona <roger@wisphub.net>
2022-11-29 14:54:12 -0500

    Update: removing class for permission

--- rest_framework_api_key/permissions.py
+++ rest_framework_api_key/permissions.py
@@ -85,11 +85,3 @@ class BaseHasAPIKey(permissions.BasePermission):
 
 class HasAPIKey(BaseHasAPIKey):
     model = APIKey
-
-
-class HasAPIAccess(permissions.BasePermission):
-    message = 'Invalid or missing API Key.'
-
-    def has_permission(self, request, view):
-        api_key = request.META.get('HTTP_API_KEY', '')
-        return APIKey.objects.filter(key=api_key).exists()
